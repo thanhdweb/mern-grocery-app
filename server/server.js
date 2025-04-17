9568089b7e32abf385920c4b423675b4418591a7
thanhdweb <thanhdweb@gmail.com>
2025-04-17 22:53:48 +0700

    project completed

--- server/server.js
+++ server/server.js
@@ -19,7 +19,10 @@ await connectDB();
 await connectCloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mern-grocery-app.vercel.app",
+];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
